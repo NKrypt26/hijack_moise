f9a5829ad604212b471d0d15dd5af5f067089fb9
Ap2626 <adpar2001@gmail.com>
2019-11-02 14:26:49 -0500

    Basic json query

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
 import requests
+import json
+import numpy as np
 
 response = requests.get("http://api.open-notify.org/astros.json")
-print(response.status_code)
-print(response.json())
+#print(response.status_code)
+todos = json.loads(response.text)
+
+people = todos["people"]
+
+for person in people:
+    print(person["name"])
+
+
