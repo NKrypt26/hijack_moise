3536b0a83241eb55a1db23e1c28dc29ce53d2a2d
Ap2626 <adpar2001@gmail.com>
2019-11-03 00:10:35 -0500

    graph

--- iex_api_test.py
+++ iex_api_test.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from datetime import timedelta
+
 from iexfinance.stocks import get_historical_data
 
 IEX_TOKEN = 'pk_59ecee438d9947bdb0d4434ea5d66114'
@@ -15,26 +17,15 @@ def makeGraph(ticker):
     plot.plot()
     return plt
 
-# from iexfinance.stocks import Stock
-#
-# def getInfo(ticker, date):
-#     #date format 'yyyy-mm-dd'
-#     import json
-#     tickerS = Stock(ticker, token=IEX_TOKEN)
-#     stockInfo = tickerS.get_quote()
-#     sheet = tickerS.get_historical_data(ticker, date, date, output_formate='pandas', token=IEX_TOKEN)
-#     info = [stockInfo['symbol'], stockInfo['companyName'], sheet]
-#     print(sheet)
-#
-# array = getInfo('AAPL', '2016-03-13')
-# print(array)
+from iexfinance.stocks import Stock
 
+def getInfo(ticker, datetime):
+    tickerS = Stock(ticker, token=IEX_TOKEN)
+    stockInfo = tickerS.get_quote()
+    sheet = get_historical_data(ticker, datetime, datetime.now() + timedelta(days=1), output_format='pandas', token=IEX_TOKEN)
+    info = [stockInfo['symbol'], stockInfo['companyName']]
+    print(sheet)
+    return info
 
-
-
-
-
-# from iexfinance.stocks import Stock
-#
-# a = Stock("AAPL", token="pk_59ecee438d9947bdb0d4434ea5d66114")
-# quote = a.get_quote()
+array = getInfo('AAPL', datetime(2016, 3, 13))
+print(array)
